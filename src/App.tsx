fcbeec7a883384314a93096d4c859a45beecc2e2
jangseyeong <jang.se.yeong000@gmail.com>
2025-11-06 02:48:54 +0900

    feat: update favicon, title

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { Flex } from 'styled-system/jsx'
-import { Tabs } from './ui-lib/components/Tabs'
+import { Flex, styled } from 'styled-system/jsx'
+import { Text } from './ui-lib/components/Text'
 
 function App() {
   return (
     <Flex direction="column" gap="16px">
-      <Tabs.Root defaultValue="tab1">
-        <Tabs.List>
-          <Tabs.Trigger value="tab1">탭 1</Tabs.Trigger>
-          <Tabs.Trigger value="tab2">탭 2</Tabs.Trigger>
-        </Tabs.List>
-        <Tabs.Content value="tab1">내용 1</Tabs.Content>
-        <Tabs.Content value="tab2">내용 2</Tabs.Content>
-      </Tabs.Root>
+      <styled.img src="/logo.svg" alt="Bikini Transport" />
+      <Text variant="H0_Bold" textAlign="center">
+        Hello, Bikini Transport
+      </Text>
     </Flex>
   )
 }
