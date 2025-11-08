8a377493ac0a46a93d1eb3063f60689f962443a6
jangseyeong <jang.se.yeong000@gmail.com>
2025-11-08 16:15:45 +0900

    add HomePage Image

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,27 @@
-import { Flex, styled } from 'styled-system/jsx'
+import { Button } from '@/ui-lib/components/Button'
+import { Box, Divider, Flex } from 'styled-system/jsx'
 import { Text } from '../../ui-lib/components/Text'
 
+// TODO: Layout 변경 필요
 export const HomePage = () => {
   return (
-    <Flex direction="column" gap="16px">
-      <styled.img src="/logo.svg" alt="Bikini Transport" />
-      <Text variant="H0_Bold" textAlign="center">
-        Hello, Bikini Transport
-      </Text>
+    <Flex direction="column" height="screen" backgroundColor="background.neutral" position="relative">
+      <Box position="absolute" top={0} left={0}>
+        <img src="/home-bg-sea.png" alt="background" />
+        <img src="/home-character-spongebob.png" alt="spongebob" />
+      </Box>
+      <Box flex="1" overflowY="auto" p={5} zIndex={1}>
+        <Box backgroundColor="background.normal" borderRadius="20px">
+          <Box>
+            출발지~도착지
+            <Divider />
+            <Text variant="H2_Bold">2024.06.15 (토) 14:00 출발</Text>
+          </Box>
+        </Box>
+      </Box>
+      <Box px={5} py={4} flexShrink={0} backgroundColor="background.normal" zIndex={1}>
+        <Button fullWidth>버스표 조회</Button>
+      </Box>
     </Flex>
   )
 }
