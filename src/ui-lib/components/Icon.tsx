95cc68f5fd61f83d6cdbac6dc2a266ba91ddeb3f
jangseyeong <jang.se.yeong000@gmail.com>
2025-11-08 14:44:31 +0900

    add LeftOutlinedIcon, fix property

--- src/ui-lib/components/Icon.tsx
+++ src/ui-lib/components/Icon.tsx
@@ -16,13 +16,7 @@ export const CheckCircleFilled = ({ size = 24, color = 'primary.normal' }: IconP
     className={css({ color })}
   >
     <circle cx="12" cy="12" r="8.5" fill="currentColor" stroke="currentColor" />
-    <path
-      d="M8 11.5L11 14.5L16 9.5"
-      stroke="#171717"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+    <path d="M8 11.5L11 14.5L16 9.5" stroke="#171717" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 )
 
@@ -36,21 +30,7 @@ export const CheckCircleOutlined = ({ size = 24, color = 'line.neutral' }: IconP
     className={css({ color })}
   >
     <circle cx="12" cy="12" r="8.5" stroke="currentColor" />
-    <path d="M8 11.5L11 14.5L16 9.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-  </svg>
-)
-
-export const CloseOutlined = ({ size = 24, color = 'label.normal' }: IconProps) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className={css({ color })}
-  >
-    <path d="M18 6L6 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
-    <path d="M18 18L6 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+    <path d="M8 11.5L11 14.5L16 9.5" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 )
 
@@ -65,5 +45,38 @@ export const SwapRightOutlined = ({ size = 16, color = 'label.normal' }: IconPro
   >
     <path d="M8 6L14 10H8V6Z" fill="currentColor" />
     <path d="M8 9V10H2V9H8Z" fill="currentColor" />
+  </svg>
+)
+
+export const LeftOutlined = ({ size = 24, color = 'label.normal' }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={css({ color })}
+  >
+    <path
+      d="M16.0002 20L8.00024 12L16.0002 4"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export const CloseOutlined = ({ size = 24, color = 'label.normal' }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={css({ color })}
+  >
+    <path d="M18 6L6 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+    <path d="M18 18L6 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
   </svg>
 )
