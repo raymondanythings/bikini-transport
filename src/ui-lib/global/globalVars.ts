17dccafd8218c922a5c34ba154930fb5c1ea67f5
jangseyeong <jang.se.yeong000@gmail.com>
2025-11-09 19:43:11 +0900

    add logo, logo horizontal, krabby-patty font

--- src/ui-lib/global/globalVars.ts
+++ src/ui-lib/global/globalVars.ts
@@ -1,4 +1,6 @@
 export const globalVars = {
   "--font-pretendard":
     "Pretendard, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+
+  "--font-krabby-patty": "Krabby Patty, sans-serif",
 };
--- src/ui-lib/theme/tokens/fonts.ts
+++ src/ui-lib/theme/tokens/fonts.ts
@@ -4,4 +4,7 @@ export const fonts = defineTokens.fonts({
   pretendard: {
     value: "var(--font-pretendard)",
   },
+  krabbyPatty: {
+    value: "var(--font-krabby-patty)",
+  },
 });
